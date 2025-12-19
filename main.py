eb045b205b1ea2a59a0e3818ad0052137dc127b1
Artur Zieliński <artur.zielinski87@gmail.com>
2025-12-19 22:30:49 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+my_dict = {'a': 1, 'b': [2, 4], 'c': 1}
+wartosci_widok = my_dict.values() # <dict_values([1, 2, 1])>
+liczba_wartosci = len(wartosci_widok) # Zwróci 3 (ilość elementów w widoku)
+print(liczba_wartosci)
